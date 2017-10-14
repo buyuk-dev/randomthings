1733edb7f95bbea1985ce8796e0e7e7e87b2db2c
Michal Michalski <buyuk.dev@gmail.com>
2017-10-14 17:00:06 +0200

    code from qualifications on PUT 2017

---
+++ puteliminacje2017/test/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+
+
+
+using namespace std;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    return 0;
+}
