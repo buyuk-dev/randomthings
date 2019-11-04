bf433188bf7028cab19f94918bcaa6111c9ec26f
Michal Michalski <buyuk.dev@gmail.com>
2019-11-04 23:11:59 +0100

    Optimize multiple signal cycles generation.
    + instead of computing all cycles repeatedly, compute one and clone it N times.
    + use numpy instead of some custom functions like drange.

--- wavegen/wavegen.py
+++ wavegen/wavegen.py
@@ -8,27 +8,11 @@
 """
 
 import argparse
-import math
-
-import numpy.fft
-import matplotlib.pyplot as pyplot
-
+import numpy
 import sounddevice
 
 
-class Config:
-    """ Common parameters which should be user-agnostic.
-    """
-    samplingFrequency = 44100  # Hz
-
-
-def drange(a, b, step):
-    """ Decimal range generator. It uses multiplication to avoid
-        cumulative error due to floating-point arithmetic.
-    """
-    dt = (b - a) / step
-    for i in range(0, int(dt)):
-        yield i * step
+SAMPLING = 44100  # Hz
 
 
 def generate_signal(frequency, cycles):
@@ -36,41 +20,44 @@ def generate_signal(frequency, cycles):
         over the length of given number of cycles. Amplitude is 
         scaled down to avoid getting over the <-1; 1> range.
     """
-    step = 1.0 / Config.samplingFrequency
-    scale = 0.9
-    print(f"generating signal at {frequency} Hz")
-    return [
-        scale * math.sin(2 * math.pi * frequency * x)
-        for x in drange(0, 2 * math.pi * cycles, step)
-    ]
+    T = 1 / frequency
+    X = numpy.linspace(0, T, T * SAMPLING)
+    Y = 0.9 * numpy.sin(2 * numpy.pi * frequency * X)
+    return numpy.tile(Y, cycles)
+
+
+def generate_spectral_signal(frequencies, cycles):
+    """ Generate normalized sum of all given frequencies.
+    """
+    F = [generate_signal(f, cycles) for f in frequencies]
+    L = [len(f) for f in F]
+    minLen = min(L)
+    X = sum(f[:minLen] for f in F)
+    return X / numpy.max(X)
+
+
+def compute_spectrum(X, cutoff=numpy.inf):
+    """ Compute normalized frequency spectrum.
+    """
+    fft = numpy.abs(numpy.fft.fft(X)) / len(X)
+    fft = fft[range(int(len(X) / 2))]
+
+    freq = numpy.fft.fftfreq(len(X), 1.0 / SAMPLING)
+    freq = freq[range(int(len(X) / 2))]
+
+    index = numpy.where(freq < cutoff)[0][-1]
+    freq = freq[:index]
+    fft = fft[:index]
+
+    return freq[1:], fft[1:]
 
 
 def configure():
     """ Configure sounddevice to use default audio
         output device at the given sampling rate.
     """
-    sounddevice.default.samplerate = Config.samplingFrequency
+    sounddevice.default.samplerate = SAMPLING
     sounddevice.default.device = sounddevice.query_devices()[1]["name"]
-
-
-def normalize(signal):
-    """ Normalize signal to the <-1; 1> range.
-    """
-    total = max(signal)
-    return [
-        2 * x / total - 1
-        for x in signal
-    ]
-
-
-def generate_spectral_signal(frequencies, cycles):
-    """ Generate normalized sum of all given frequencies.
-    """
-    X = [
-        generate_signal(freq, cycles)
-        for freq in frequencies
-    ]
-    return normalize([sum(s) for s in zip(*X)])
 
 
 def play(frequency, length):
@@ -90,19 +77,6 @@ def play_frequency_spectrum(low, high, cycles):
     print("playing signal...")
     sounddevice.play(signal)
     sounddevice.wait()
-
-
-def compute_spectrum(X):
-    """ Compute normalized frequency spectrum.
-    """
-    FFT = numpy.abs(numpy.fft.fft(X)) / len(X)
-    FFT = FFT[range(int(len(X) / 2))]
-
-    freq = numpy.fft.fftfreq(len(X), 1.0/Config.samplingFrequency)
-    freq = freq[range(int(len(X) / 2))]
-
-    return freq, FFT
-
 
 
 if __name__ == "__main__":
