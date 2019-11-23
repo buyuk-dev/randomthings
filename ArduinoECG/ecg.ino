a41126693647434daf3b49401295a4b840a1503a
Michal Michalski <buyuk.dev@gmail.com>
2019-11-23 22:01:17 +0100

    Fix invalid interpretation of arduino data.

--- ArduinoECG/ecg.ino
+++ ArduinoECG/ecg.ino
@@ -1,9 +1,33 @@
+/**
+ * Streaming analog signal from Arduino UNO via serial port. 
+ *
+ * sizeof(int) == 2B
+ * byte order: little endian
+ */
+
+
+/**
+ * Configure Arduino.
+ */
 void setup() {
   Serial.begin(9600);
   pinMode(A0, INPUT);
 }
 
+
+/**
+ * Simulating analog input signal.
+ */
+int sine_wave(double frequency) {
+  double t = double(millis()) / 1000.0;
+  return 1024.0 * sin(2.0 * 3.1415 * frequency * t);
+}
+
+
+/**
+ * Main program loop.
+ */
 void loop() {
   int sample = analogRead(A0);
-  Serial.write(sample);
+  Serial.write(reinterpret_cast<char*>(&sample), 2);
 }
